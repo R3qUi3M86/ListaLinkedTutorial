5ded8455cf47098f2d475ad671574bfb16a014a0
Iurii <ikariyuu@gmail.com>
2022-03-20 11:27:12 +0100

    Easy length()

--- src/com/linkTutorial/ListaElementow.java
+++ src/com/linkTutorial/ListaElementow.java
@@ -2,8 +2,10 @@ package com.linkTutorial;
 
 public class ListaElementow<T> {
     Elem<T> firstElement = null;
+    private int size = 0;
 
     public void add(T value){
+        size++;
         Elem<T> current = firstElement;
         if (current == null){
             firstElement = new Elem<T>();
@@ -16,6 +18,11 @@ public class ListaElementow<T> {
             current.getNextElement().setValue(value);
         }
     }
+
+    public int length() {
+        return size;
+    }
+    
 
     public Elem<T> getFirstElement(){
         return firstElement;
